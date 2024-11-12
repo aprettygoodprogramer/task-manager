0f4efdc9926babb69601b8597920aea2a91ea577
Evan <evan.j.ducas@gmail.com>
2024-11-12 18:55:02 -0500

    edited the config file again to eptry out DIR

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
+  root: '.', 
   build: {
-    outDir: 'dist',
+    outDir: 'dist', 
+    emptyOutDir: true, 
   },
   plugins: [react()],
 });
