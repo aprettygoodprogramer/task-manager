87a8ae264fc86e6d0dac34352e42940829d74e9e
Evan <evan.j.ducas@gmail.com>
2024-11-12 07:15:57 -0500

    changes

--- src/componets/loginTextInput.tsx
+++ src/componets/loginTextInput.tsx
@@ -1,9 +1,26 @@
-
 import React, { useState, ChangeEvent, FormEvent } from 'react';
+import styled from 'styled-components';
 
 interface LoginTextInputProps {
   onSubmit: (username: string, password: string) => void;
 }
+
+const CenteredContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
 
 const LoginTextInput: React.FC<LoginTextInputProps> = ({ onSubmit }) => {
   const [username, setUsername] = useState<string>('');
@@ -25,21 +42,23 @@ const LoginTextInput: React.FC<LoginTextInputProps> = ({ onSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={username}
-        onChange={handleUsernameChange}
-        placeholder="Username"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={handlePasswordChange}
-        placeholder="Password"
-      />
-      <button type="submit">Login</button>
-    </form>
+    <CenteredContainer>
+      <StyledForm onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={handleUsernameChange}
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </StyledForm>
+    </CenteredContainer>
   );
 };
 
